eb6c42d93254cffdc4aeabb0d5e2bc078d57c656
rj singh <85516771+hitorjsingh@users.noreply.github.com>
2024-01-13 03:57:26 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,15 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+const auth = getAuth();
+
+// Listen for authentication state changes
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, you can add logic for authenticated user
+    console.log('User is signed in:', user);
+  } else {
+    // No user is signed in, you can add logic for unauthenticated user
+    console.log('No user is signed in.');
+  }
+});
